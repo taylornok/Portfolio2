830ddf8265b9ddd508443d72cc8e60a2788472e4
taylornok <taylorowenskees@outlook.com>
2018-12-14 19:41:08 -0800

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,118 +1,212 @@
-jQuery(window).on('load', function() {
-	"use strict";
-    
-    
-    // HIDE PRELOADER
-    $(".preloader").addClass("hide-preloader");   
-    
-    // SHOW/ANIMATE ANIMATION CONTAINER
-    setTimeout(function(){
+"use strict";
 
-        $("#intro .animation-container").each(function() {
+//Rage Template
+//Designerd by: http://bootstrapthemes.co
 
-            var e = $(this);
+jQuery(document).ready(function ($) {
 
-            setTimeout(function(){
+//for Preloader
 
-                e.addClass("run-animation");
-
-            }, e.data("animation-delay") );
-
-        });
-
-    }, 700 );
-
-    
-});
-
-
-jQuery(document).ready(function($) {
-	"use strict";
-    
-    
-    // SMOOTH SCROLL FOR SAME PAGE LINKS
-    $(document).on('click', 'a.smooth-scroll', function(event) {
-        
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
-        }, 500);
-        
+    $(window).load(function () {
+        $("#loading").fadeOut(500);
     });
-    
-    
-    // SCROLL REVEAL SETUP
-    window.sr = ScrollReveal();
-    sr.reveal(".scroll-animated", { 
-        duration: 600,
-        delay: 0,
-        origin: "left",
-        rotate: { x: 0, y: 0, z: 0 },
-        opacity: 0,
-        distance: "20vh",
-        viewFactor: 0.4,
-        scale: 1,
-    });
-    
-    
-    // AJAX CONTACT FORM SUBMIT
-    $("#contact-form").submit(function(e) {
 
-        e.preventDefault();
-        var postdata = $(this).serialize();
 
-        $.ajax({
-
-            type: "POST",
-            url: "assets/php/contact.php",
-            data: postdata,
-            dataType: "json",
-            success: function(json) {
-
-                $("#contact-form input, #contact-form textarea").removeClass("error");
-
-                setTimeout(function(){
-
-                    if (json.nameMessage !== "") {
-
-                        $("#contact-form-name").addClass("error");
-
-                    }
-
-                    if (json.emailMessage !== "") {
-
-                        $("#contact-form-email").addClass("error");
-
-                    }
-
-                    if (json.messageMessage !== "") {
-
-                        $("#contact-form-message").addClass("error");
-
-                    }
-
-                }, 10);
-
-                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
-
-                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
-                    $('#contact-form').addClass("success");
-                    $('#contact-form textarea, #contact-form input').val("");
-                    
-                    setTimeout(function(){
-                        
-                        $('#contact-form').removeClass("success");
-                        
-                    },4000);
-
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 80)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
                 }
-
+                return false;
             }
-
-        });
-
+        }
     });
 
-    
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+    var wow = new WOW({
+        mobile: false // trigger animations on mobile devices (default is true)
+    });
+    wow.init();
+
+// magnificPopup
+
+    $('.popup-img').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    $('.video-link').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        iframe: {
+  markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+  patterns: {
+    youtube: {
+      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+      // Or null - full URL will be returned
+      // Or a function that should return %id%, for example:
+      // id: function(url) { return 'parsed id'; }
+
+      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+    }
+    // you may add here more sources
+
+  },
+
+  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+}
+ 
+    });
+
+
+
+// slick slider active Home Page Tow
+    $(".testimonial_slid").slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: "<i class='fa fa-angle-left nextprevleft'></i>",
+        nextArrow: "<i class='fa fa-angle-right nextprevright'></i>",
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+
+
+//    featured slider
+    $('.featured_slider').slick({
+        centerMode: true,
+        dote: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        speed: 1500,
+        index: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+
+//---------------------------------------------
+// Counter 
+//---------------------------------------------
+
+    $('.statistic-counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+//---------------------------------------------
+// Scroll Up 
+//---------------------------------------------
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+
+
+//About us accordion 
+
+    $("#faq_main_content").collapse({
+        accordion: true,
+        open: function () {
+            this.addClass("open");
+            this.css({height: this.children().outerHeight()});
+        },
+        close: function () {
+            this.css({height: "0px"});
+            this.removeClass("open");
+        }
+    });
+
+
+
+
+
+//Team Skillbar active js
+
+    jQuery('.teamskillbar').each(function () {
+        jQuery(this).find('.teamskillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        }, 6000);
+    });
+
+
+
+//Span Menu toggle
+	$(".navbar-header button").on('click', function(){
+		$('.navbar-header button').toggleClass('active');
+	});
+
+  
+
+    //End
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
